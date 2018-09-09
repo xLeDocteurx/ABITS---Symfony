34d78eac93ae4777080eec30cd23568dc6c5b2fc
xLeDocteurx <lenoirc.fr@gmail.com>
2018-09-09 20:46:22 +0200

    fixed something in the usersForm

--- src/Form/TagsType.php
+++ src/Form/TagsType.php
@@ -2,30 +2,24 @@
 
 namespace App\Form;
 
-use App\Entity\Users;
+use App\Entity\Tags;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UsersType extends AbstractType
+class TagsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('country')
-            ->add('email')
-            ->add('password')
-            ->add('bio')
-            // ->add('confirmed')
-            // ->add('bottlesSents')
+            ->add('word')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Users::class,
+            'data_class' => Tags::class,
         ]);
     }
 }
