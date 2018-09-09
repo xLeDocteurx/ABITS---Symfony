0294d98e9ef14871901d3e4bc6e09740849a2d0b
xLeDocteurx <lenoirc.fr@gmail.com>
2018-09-09 20:51:41 +0200

    fixed the request for received bottles

--- src/Controller/BottlesSentController.php
+++ src/Controller/BottlesSentController.php
@@ -41,7 +41,7 @@ class BottlesSentController extends Controller
         ->findOneByUsername('LeDocteur');
         
         // $user_bottles = $bottlesSentRepository->findAll();
-        $user_bottles = $bottlesSentRepository->findByAuthor($thisUser);
+        $user_bottles = $bottlesSentRepository->findById($thisUser->getId());
         // $user_bottles = $bottlesSentRepository->findBy(
         //     array('receivers' => $thisUser) // Critere
         //     // array('date' => 'desc'),        // Tri
